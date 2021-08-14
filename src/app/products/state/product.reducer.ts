297170e806d3c8af5609b6c0911e5430ef8f3b09
Ty Ponzo <52117423+tponzo@users.noreply.github.com>
2021-08-14 12:46:34 -0500

    Product Selectors

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,8 +1,43 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
+import { Product } from '../product';
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), state => {
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+};
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+export const productReducer = createReducer<ProductState>(
+  initialState,
+  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode
